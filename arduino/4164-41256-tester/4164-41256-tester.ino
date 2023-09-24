ed6e4854ca6b6ab056c4266efddc177887b7a69a
Boris Popov <popov@whitekefir.ru>
2023-09-24 15:01:58 +0300

    remove EPROM operation

--- arduino/4164-41256-tester/4164-41256-tester.ino
+++ arduino/4164-41256-tester/4164-41256-tester.ino
@@ -1,8 +1,6 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
-#include <SPI.h>
 #include <Wire.h>
-#include <EEPROM.h>
 
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0); 
 
@@ -53,158 +51,155 @@ String  m_sym[2] = {"\\","/"};
 
 volatile int bus_size;
 const unsigned int a_bus[BUS_SIZE] = {
-  XA0, XA1, XA2, XA3, XA4, XA5, XA6, XA7, XA8
+		 XA0, XA1, XA2, XA3, XA4, XA5, XA6, XA7, XA8
 };
 
 
 /* -----  SETUP  ------ */
- void setup() {
-  int i;
-   u8g2.begin();
-   Serial.begin(9600); 
+void setup() {
+		 int i;
+		 u8g2.begin();
+		 Serial.begin(9600); 
 
-for (i = 0; i < BUS_SIZE; i++)
-    pinMode(a_bus[i], OUTPUT);
+		 for (i = 0; i < BUS_SIZE; i++)
+					pinMode(a_bus[i], OUTPUT);
 
-  pinMode(CAS, OUTPUT);
-  pinMode(RAS, OUTPUT);
-  pinMode(WE, OUTPUT);
-  pinMode(DI, OUTPUT);
-  //pinMode(M_CHIPSEL, INPUT);
-  pinMode(M_START, INPUT);
-  pinMode(DO, INPUT);
-  digitalWrite(WE, HIGH);
-  digitalWrite(RAS, HIGH);
-  digitalWrite(CAS, HIGH);
+		 pinMode(CAS, OUTPUT);
+		 pinMode(RAS, OUTPUT);
+		 pinMode(WE, OUTPUT);
+		 pinMode(DI, OUTPUT);
+		 //pinMode(M_CHIPSEL, INPUT);
+		 pinMode(M_START, INPUT);
+		 pinMode(DO, INPUT);
+		 digitalWrite(WE, HIGH);
+		 digitalWrite(RAS, HIGH);
+		 digitalWrite(CAS, HIGH);
 
-  mode =EEPROM.read(0);//memoria 4164-256-32l-32h
-  if (mode>3){mode=0;}
 
 /*
-if (digitalRead(M_TYPE)) {
-    /* jumper not set - 41256 */
-    /*bus_size = BUS_SIZE;
-    //Serial.print("256Kx1 ");
-  } else {
-    /* jumper set - 4164 */
-    /*bus_size = BUS_SIZE - 1;
-    //Serial.print("64Kx1 ");
-  }*/
-  selector=selchip();
-  l_sel=selector;
-  setDisp();\
+	if (digitalRead(M_TYPE)) {
+	/* jumper not set - 41256 */
+		 /*bus_size = BUS_SIZE;
+		 //Serial.print("256Kx1 ");
+		 } else {
+		 /* jumper set - 4164 */
+		 /*bus_size = BUS_SIZE - 1;
+		 //Serial.print("64Kx1 ");
+		 }*/
+		 selector=selchip();
+		 l_sel=selector;
+		 setDisp();\
 }
 /* -----------  LOOP  ---------- */
-  void loop(void) {
+void loop(void) {
 
-    getserial();
+		 getserial();
 
-    selector=selchip(); // 0= vuoto 1=4164 2=4116
+		 selector=selchip(); // 0= vuoto 1=4164 2=4116
 
-    if (l_sel!=selector) {
-      l_sel=selector;
-      setDisp();
-    }
+		 if (l_sel!=selector) {
+					l_sel=selector;
+					setDisp();
+		 }
       
-   //change mode - start
-   currentState = digitalRead(M_START);
-  if (lastState == HIGH && currentState == LOW) { // button is pressed
-    pressedTime = millis();
-    lastState = LOW;
-  }
-  if(lastState == LOW && currentState == HIGH) { // button is released
-    lastState = HIGH;
-    long pressDuration = millis() - pressedTime;
-    if (pressDuration < 1500 ) { //short press chenge mode
-      if (selector==1 ){
-        mode++;
-        if (mode > 3) mode=0;
-        EEPROM.write(0, mode);
-        setDisp();
-      }
-    }
-  }
-    if(lastState == LOW && currentState == LOW &&  millis() - pressedTime > 1500) {
-    //else { //long press start
-      isError=false;
-      ntest=0;
-      if (selector>0) startTest();
-  }
+		 //change mode - start
+		 currentState = digitalRead(M_START);
+		 if (lastState == HIGH && currentState == LOW) { // button is pressed
+					pressedTime = millis();
+					lastState = LOW;
+		 }
+		 if(lastState == LOW && currentState == HIGH) { // button is released
+					lastState = HIGH;
+					long pressDuration = millis() - pressedTime;
+					if (pressDuration < 1500 ) { //short press chenge mode
+							 if (selector==1 ){
+										mode++;
+										if (mode > 3) mode=0;
+										setDisp();
+							 }
+					}
+		 }
+		 if(lastState == LOW && currentState == LOW &&  millis() - pressedTime > 1500) {
+					//else { //long press start
+					isError=false;
+					ntest=0;
+					if (selector>0) startTest();
+		 }
    
-   delay(100);
+		 delay(100);
 }
 
    
 void startTest() {
-   initDram();
-   u8g2.clearBuffer();
-   u8g2.setFont(u8g2_font_logisoso16_tr);
-   u8g2.drawStr(8,16,"Test no.1");
-   u8g2.setFont(u8g2_font_gb16st_t_2);
-   switch (selector) {
-    case 2:
-    u8g2.drawStr(4,32,">16k");
-    bus_size = BUS_SIZE - 2;
-    break;
-    case 1:
-      if (mode == 0){
-      u8g2.drawStr(4,32,">256k");
-      bus_size = BUS_SIZE;
-      } else {
-      bus_size = BUS_SIZE - 1;
-      }
-      if (mode == 1) u8g2.drawStr(4,32,">64k");
-      if (mode == 2) u8g2.drawStr(4,32,">32k Low");
-      if (mode == 3) u8g2.drawStr(4,32,">32k High");
-   }
+		 initDram();
+		 u8g2.clearBuffer();
+		 u8g2.setFont(u8g2_font_logisoso16_tr);
+		 u8g2.drawStr(8,16,"Test no.1");
+		 u8g2.setFont(u8g2_font_gb16st_t_2);
+		 switch (selector) {
+		 case 2:
+					u8g2.drawStr(4,32,">16k");
+					bus_size = BUS_SIZE - 2;
+					break;
+		 case 1:
+					if (mode == 0){
+							 u8g2.drawStr(4,32,">256k");
+							 bus_size = BUS_SIZE;
+					} else {
+							 bus_size = BUS_SIZE - 1;
+					}
+					if (mode == 1) u8g2.drawStr(4,32,">64k");
+					if (mode == 2) u8g2.drawStr(4,32,">32k Low");
+					if (mode == 3) u8g2.drawStr(4,32,">32k High");
+		 }
 
-  u8g2.sendBuffer();
-  interrupts(); noInterrupts(); 
+		 u8g2.sendBuffer();
+		 interrupts(); noInterrupts(); 
   
-  fillx(0);
-  if (!isError){
-    resDisp("Test no.2");
-    interrupts();u8g2.sendBuffer(); noInterrupts(); 
-    fillx(1);
-  }
-  if (!isError){
-    interrupts(); 
-    resDisp("Full Write...");
-    u8g2.sendBuffer();
-    noInterrupts(); 
-    fill(1,false);
-  }
-  if (!isError){
-    interrupts(); 
-    resDisp("Full Read...");
-    u8g2.sendBuffer();
-    noInterrupts(); 
-    readonly(1);
-  }
-    if (!isError){
-   resDisp("Finishing...");
-    interrupts();u8g2.sendBuffer(); noInterrupts(); 
-    fill(0,true);
-  }
+		 fillx(0);
+		 if (!isError){
+					resDisp("Test no.2");
+					interrupts();u8g2.sendBuffer(); noInterrupts(); 
+					fillx(1);
+		 }
+		 if (!isError){
+					interrupts(); 
+					resDisp("Full Write...");
+					u8g2.sendBuffer();
+					noInterrupts(); 
+					fill(1,false);
+		 }
+		 if (!isError){
+					interrupts(); 
+					resDisp("Full Read...");
+					u8g2.sendBuffer();
+					noInterrupts(); 
+					readonly(1);
+		 }
+		 if (!isError){
+					resDisp("Finishing...");
+					interrupts();u8g2.sendBuffer(); noInterrupts(); 
+					fill(0,true);
+		 }
 
-  interrupts();
+		 interrupts();
   
-  if (!isError){
-   finish();
-   /*
-   u8g2.clearBuffer();
-   u8g2.setFont(u8g2_font_logisoso16_tr);
-   u8g2.drawStr(6,16,"TEST PASSED");
-   u8g2.setFont(u8g2_font_gb16st_t_2);
-   u8g2.drawStr(12,30,"press start");
-   u8g2.sendBuffer();*/
-         while (digitalRead(M_START)==LOW) {
-      }
+		 if (!isError){
+					finish();
+					/*
+						u8g2.clearBuffer();
+						u8g2.setFont(u8g2_font_logisoso16_tr);
+						u8g2.drawStr(6,16,"TEST PASSED");
+						u8g2.setFont(u8g2_font_gb16st_t_2);
+						u8g2.drawStr(12,30,"press start");
+						u8g2.sendBuffer();*/
+					while (digitalRead(M_START)==LOW) {
+					}
           while (digitalRead(M_START)==HIGH) {
-      }
-      setDisp();
-      delay(300);
-  } else {
-    setDisp();
-  }
+					}
+					setDisp();
+					delay(300);
+		 } else {
+					setDisp();
+		 }
 }
