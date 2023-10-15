219057b3f66c2147948042b9049126dfac41803f
Boris Popov <popov@whitekefir.ru>
2023-10-15 18:09:17 +0300

    remove char.ino

--- arduino/4164-41256-tester/char.ino
+++
@@ -1,8 +0,0 @@
-void finish() {
-   u8g2.clearBuffer();
-   u8g2.setFont(u8g2_font_logisoso16_tr);
-   u8g2.drawStr(6,16,"TEST PASSED");
-   u8g2.setFont(u8g2_font_gb16st_t_2);
-   u8g2.drawStr(12,30,"press start");
-   u8g2.sendBuffer();
-}
--- arduino/4164-41256-tester/function.ino
+++ arduino/4164-41256-tester/function.ino
@@ -204,3 +204,12 @@ void resDisp (String ntest) {
 					if (mode == 3) u8g2.drawStr(4,32,">32K High");
 		 }
 }
+
+void finish() {
+   u8g2.clearBuffer();
+   u8g2.setFont(u8g2_font_logisoso16_tr);
+   u8g2.drawStr(6,16,"TEST PASSED");
+   u8g2.setFont(u8g2_font_gb16st_t_2);
+   u8g2.drawStr(12,30,"press start");
+   u8g2.sendBuffer();
+}
