ed31bed8bf664e61d9bb9de90a93883a75e48cc2
Boris Popov <popov@whitekefir.ru>
2023-10-21 16:34:58 +0300

    add impl for Buttons::wait_ok

--- arduino/4164-41256-tester/Buttons.cpp
+++ arduino/4164-41256-tester/Buttons.cpp
@@ -3,10 +3,17 @@
 //
 
 #include "Buttons.hpp"
+#include <Arduino.h>
 #include <stddef.h>
 
 Button* Buttons::poll() {
      if (sel.status() == Button::Release) return &sel;
      if (ok.status() == Button::Release) return &ok;
      return NULL;
+}
+
+void Buttons::wait_ok() {
+     while (ok.status() != Button::Release) {
+	  delay(100);
+     }
 }
